bd48f564951f9af228748c3aefb35810a3950c49
AnHuynh341 <Meochon341@gmail.com>
2025-04-11 19:12:15 +0700

    added stuffs

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,39 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font_with_fallback {
+    "JetBrainsMono Nerd Font",
+    "Fira Code",
+    "monospace",
+  },
+
+  keys = {
+    {
+      key = "v",
+      mods = "CTRL",
+      action = wezterm.action.PasteFrom("Clipboard"),
+    },
+  },
+
+  font_size = 12,
+  color_scheme = "Dracula", -- Or "Dracula", "Gruvbox Dark", etc.
+  colors = {
+    background = "#000000",  -- override just the background
+  },
+  enable_tab_bar = false,           -- Hide tabs if you prefer a clean look
+  use_fancy_tab_bar = false,
+  hide_mouse_cursor_when_typing = false,
+
+  window_padding = {
+    left = 8,
+    right = 8,
+    top = 6,
+    bottom = 6,
+  },
+
+  default_prog = { "zsh", "-l" },
+
+  enable_wayland = true,
+
+}
+
